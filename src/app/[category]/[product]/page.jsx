84995ac043dcca43b75f8290b5826ef3cb583eeb
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-07-29 19:13:14 +0530

    Implement logic for changing product image

--- src/app/[category]/[product]/page.jsx
+++ src/app/[category]/[product]/page.jsx
@@ -1,66 +1,51 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 import { AiFillStar, AiOutlineHeart } from "react-icons/ai";
 import { PiShoppingBagLight } from "react-icons/pi";
 
 function Product() {
+  const productImages = [
+    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10307423/2019/11/7/7f8bf98e-96b3-490c-9512-dad6a7279feb1573110418783-Roadster-Men-Tshirts-241573110416534-1.jpg",
+    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10307423/2019/11/7/6700b1db-63b4-4e2c-b937-48a52277417b1573110418700-Roadster-Men-Tshirts-241573110416534-3.jpg",
+    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10307423/2019/11/7/2cea7c8a-57f6-4f94-aa7e-bbd3ed98029c1573110418656-Roadster-Men-Tshirts-241573110416534-4.jpg",
+    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10307423/2019/11/7/181f9191-76a2-49e6-92da-e016238379281573110418580-Roadster-Men-Tshirts-241573110416534-5.jpg",
+    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10307423/2019/11/7/5058ade6-1476-4bc1-ad8c-bbb9bb5653d11573110418743-Roadster-Men-Tshirts-241573110416534-2.jpg",
+  ];
+
+  const [currMainImage, setCurrMainImage] = useState(0);
+
   return (
     <div className="flex flex-col md:flex-row md:mx-10 md:gap-7 md:mt-10">
       <div className="flex flex-col md:flex-row-reverse md:flex-1 md:gap-5">
         <div className="w-full h-[500px] md:h-auto md:w-[85%]">
           <Image
-            src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
+            src={productImages[currMainImage]}
             width={0}
             height={0}
-            sizes="100vw"
+            sizes="100vw, (min-width: 768px) 50vw"
             className="w-full h-full object-cover md:object-contain md:object-top"
           />
         </div>
         <div className="flex w-full gap-5 mt-5 px-3 md:flex-col md:w-[15%] md:mt-0 md:px-0">
-          <div className="flex-1 md:flex-none">
-            <Image
-              src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
-              width={0}
-              height={0}
-              sizes="20vw"
-              className="w-full"
-            />
-          </div>
-          <div className="flex-1 md:flex-none">
-            <Image
-              src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
-              width={0}
-              height={0}
-              sizes="20vw"
-              className="w-full"
-            />
-          </div>
-          <div className="flex-1 md:flex-none">
-            <Image
-              src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
-              width={0}
-              height={0}
-              sizes="20vw"
-              className="w-full"
-            />
-          </div>
-          <div className="flex-1 md:flex-none">
-            <Image
-              src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
-              width={0}
-              height={0}
-              sizes="20vw"
-              className="w-full"
-            />
-          </div>
-          <div className="flex-1 md:flex-none">
-            <Image
-              src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11687254/2020/6/16/c0336423-1c27-46b7-9257-e90ae0052f881592300967435-Roadster-Men-Tshirts-4271592300965660-1.jpg"
-              width={0}
-              height={0}
-              sizes="20vw"
-              className="w-full"
-            />
-          </div>
+          {productImages?.map((image, idx) => (
+            <div
+              className={`flex-1 md:flex-none ${
+                currMainImage == idx ? "border-2 border-sky-400" : ""
+              }`}
+              key={idx}
+              onClick={() => setCurrMainImage(idx)}
+            >
+              <Image
+                src={image}
+                width={0}
+                height={0}
+                sizes="20vw"
+                className="w-full"
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className="px-3 mt-5 md:flex-1 md:mt-0">
@@ -113,7 +98,9 @@ function Product() {
         </div>
 
         <div className="flex flex-col gap-3 mt-10 lg:text-lg">
-          <h2 className="font-bold text-lg lg:text-xl uppercase">Product details</h2>
+          <h2 className="font-bold text-lg lg:text-xl uppercase">
+            Product details
+          </h2>
           <p>
             White and Mustard yellow colourblocked T-shirt, has a round neck,
             and short sleeves
