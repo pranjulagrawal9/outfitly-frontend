a8873ba9dbc4e33d691b6506cd892d5891f63e48
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-08-19 12:50:11 +0530

    Fetch products on home page using graphql and fix initialization issue with swiper-container

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
           {
             hostname: 'assets.myntassets.com',
           },
+          {
+            hostname: '127.0.0.1',
+          },
         ],
       },
 }
--- src/app/components/ProductsSlider.jsx
+++ src/app/components/ProductsSlider.jsx
@@ -1,41 +1,68 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { register } from "swiper/element/bundle";
 register();
+import { useQuery, gql } from "@apollo/client";
 
 function ProductsSlider({ title }) {
   const swiperContainerRef = useRef(null);
-  const [swiperInitialized, setswiperInitialized] = useState(false);
+  const filterByObj =
+    title === "BestSellers"
+      ? { bestseller: { eq: true } }
+      : { trending: { eq: true } };
+  const QUERY = gql`
+    query GetProducts($filterByObj: ProductFiltersInput!) {
+      products(filters: $filterByObj) {
+        data {
+          id
+          attributes {
+            images {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            title
+          }
+        }
+      }
+    }
+  `;
+
+  const { data, loading, error } = useQuery(QUERY, {
+    variables: { filterByObj },
+  });
+  const products = data?.products?.data;
 
   useEffect(() => {
     const swiperEl = swiperContainerRef.current;
-    Object.assign(swiperEl, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      navigation: true,
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
+    if (products) {
+      Object.assign(swiperEl, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: true,
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
         },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      },
-    });
-    swiperEl.addEventListener("init", (e) => {
-      setswiperInitialized(true);
-    });
+      });
 
-    swiperEl.initialize();
-  }, []);
+      swiperEl.initialize();
+    }
+  }, [products]);
 
   return (
     <section className="mt-10">
@@ -48,31 +75,25 @@ function ProductsSlider({ title }) {
           class="w-full h-full p-2"
           ref={swiperContainerRef}
         >
-          {swiperInitialized &&
-            Array(7)
-              .fill(
-                "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/9793511/2019/6/15/37ac1de4-2d8c-4e18-a431-a89cdf4695dd1560580175558-Roadster-Men-Black-Solid-Round-Neck-T-shirt-8361560580174560-1.jpg"
-              )
-              .map((item) => (
-                <swiper-slide>
-                  <div className="w-full h-full">
-                    <Image
-                      src={item}
-                      width={0}
-                      height={0}
-                      sizes="100vw, (min-width: 640px) 50vw, (min-width: 768) 25vw, (min-width: 1024px) 20vw"
-                      style={{
-                        objectFit: "contain",
-                        width: "100%",
-                        height: "80%",
-                      }}
-                    />
-                    <h2 className="text-center text-lg font-medium mt-5 md:mt-0">
-                      Roadster Men Black Pure Cotton T-shirt
-                    </h2>
-                  </div>
-                </swiper-slide>
-              ))}
+          {products?.map((product) => (
+            <swiper-slide key={product.id}>
+              <Image
+                src={`http://127.0.0.1:1337${product.attributes.images.data[0].attributes.url}`}
+                width={0}
+                height={0}
+                alt={product.attributes.title}
+                sizes="100vw, (min-width: 640px) 50vw, (min-width: 768) 25vw, (min-width: 1024px) 20vw"
+                style={{
+                  objectFit: "contain",
+                  width: "100%",
+                  height: "80%",
+                }}
+              />
+              <h2 className="text-center text-lg font-medium mt-5 md:mt-0">
+                {product.attributes.title}
+              </h2>
+            </swiper-slide>
+          ))}
         </swiper-container>
       </div>
     </section>
