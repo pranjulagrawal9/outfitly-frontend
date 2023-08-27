40d5d7c4b4f9fa90965b341ec2e5ec6e7bd6a083
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-08-27 16:36:01 +0530

    Fetch Carousel images on home page using graphql

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -1,42 +1,57 @@
 "use client";
 
+import { gql, useQuery } from "@apollo/client";
 import Image from "next/image";
-import { useRef, useEffect } from "react";
 import { register } from "swiper/element/bundle";
 register();
 
 function Slider() {
-  const slides = [
-    "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/84b6a214-9eb3-49eb-9f9d-72cec56ec5d71659019908592-Indian-Wear_DK--1-.jpg",
-    "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/6107d28b-2bcb-44e6-9743-655b54550b8f1659020199598-Workwear_Desk--1-.jpg",
-    "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/179e278f-77ee-44c2-bf39-9f00b0cd08e01658752429301-Handbags_Desk.jpg",
-  ];
-  const swiperElRef = useRef(null);
+  const GetSliderImages = gql`
+    {
+      sliders {
+        data {
+          attributes {
+            images {
+              data {
+                id
+                attributes {
+                  url
+                  alternativeText
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+  const { data } = useQuery(GetSliderImages);
 
   return (
     <div className="h-40 md:h-60 lg:h-80 xl:h-96">
-      <swiper-container
-        class="w-full h-full relative"
-        ref={swiperElRef}
-        navigation="true"
-        pagination="true"
-        pagination-clickable="true"
-        loop="true"
-        autoplay-delay="2500"
-        autoplay-disable-on-interaction="false"
-      >
-        {slides.map((slide, idx) => (
-          <swiper-slide key={idx} data-swiper-slide-index={idx}>
-            <Image
-              src={slide}
-              fill="true"
-              alt="carousel"
-              className="block w-full h-full object-cover"
-              priority="true"
-            />
-          </swiper-slide>
-        ))}
-      </swiper-container>
+      {data && (
+        <swiper-container
+          class="w-full h-full relative"
+          navigation="true"
+          pagination="true"
+          pagination-clickable="true"
+          loop="true"
+          autoplay-delay="2500"
+          autoplay-disable-on-interaction="false"
+        >
+          {data?.sliders.data[0].attributes.images.data.map((slide) => (
+            <swiper-slide key={slide.id}>
+              <Image
+                src={`http://127.0.0.1:1337${slide.attributes.url}`}
+                fill="true"
+                alt={slide.attributes.alternativeText}
+                className="block w-full h-full object-cover"
+                priority="true"
+              />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      )}
     </div>
   );
 }
